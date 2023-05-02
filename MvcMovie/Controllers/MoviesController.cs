c087fca9cae432d292ec02ec86d53a905820b3ec
Robert J <106358887+onemanarmy45@users.noreply.github.com>
2023-05-02 12:35:37 -0400

    mvc movie

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -52,8 +52,6 @@ namespace MvcMovie.Controllers
         }
 
         // POST: Movies/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
@@ -84,8 +82,6 @@ namespace MvcMovie.Controllers
         }
 
         // POST: Movies/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "A Beautiful Mind",
+                    ReleaseDate = DateTime.Parse("12-13-2001"),
+                    Genre = "Drama",
+                    Price = 14M
+                },
+                new Movie
+                {
+                    Title = "Ferris Bueller's Day Off",
+                    ReleaseDate = DateTime.Parse("06-11-1986"),
+                    Genre = "Comedy",
+                    Price = 14M
+                },
+                new Movie
+                {
+                    Title = "Zodiac",
+                    ReleaseDate = DateTime.Parse("03-02-2007"),
+                    Genre = "Thriller",
+                    Price = 12M
+                },
+                new Movie
+                {
+                    Title = "Juno",
+                    ReleaseDate = DateTime.Parse("12-05-2007"),
+                    Genre = "Drama",
+                    Price = 12M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
